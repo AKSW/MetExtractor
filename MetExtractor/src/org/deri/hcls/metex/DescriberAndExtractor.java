4906c4c44d0f56313d61c60893ba404da052e252
Natanael Arndt <arndtn@gmail.com>
2014-03-10 13:22:58 +0000

    Use configuration

--- MetExtractor/src/org/deri/hcls/metex/DescriberAndExtractor.java
+++ MetExtractor/src/org/deri/hcls/metex/DescriberAndExtractor.java
@@ -13,6 +13,9 @@ import java.util.Set;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.math.RandomUtils;
 import org.apache.jena.riot.RiotException;
+import org.deri.hcls.Configuration;
+import org.deri.hcls.Configuration.VirtuosoConfiguration;
+import org.deri.hcls.HelpException;
 import org.deri.hcls.QueryExecutionException;
 import org.deri.hcls.ResourceHelper;
 import org.deri.hcls.metex.adapters.DatahubAdapter;
@@ -26,8 +29,12 @@ import org.deri.hcls.vocabulary.Vocabularies;
 import virtuoso.jdbc4.VirtuosoDataSource;
 import virtuoso.jena.driver.VirtModel;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.query.QueryException;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
@@ -43,6 +50,20 @@ import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
 
 public class DescriberAndExtractor {
+	public static final String CONF_NS = "http://metex.hcls.deri.org/voc/conf/";
+	private static OntModel ontModel = ModelFactory.createOntologyModel(
+			OntModelSpec.OWL_MEM, null);
+	public static final Property CONF_baseUri = ontModel.createProperty(CONF_NS
+			+ "baseUri");
+	public static final Property CONF_owBaseUri = ontModel
+			.createProperty(CONF_NS + "owBaseUri");
+	public static final Property CONF_endpointsFetchLimit = ontModel
+			.createProperty(CONF_NS + "endpointFetchLimit");
+	public static final Property CONF_endpointsPrintLists = ontModel
+			.createProperty(CONF_NS + "endpointsPrintLists");
+	public static final Property CONF_endpoint = ontModel
+			.createProperty(CONF_NS + "endpoint");
+	
 
 	private VirtuosoDataSource ds;
 	private Model siteModel;
@@ -51,36 +72,51 @@ public class DescriberAndExtractor {
 	private Map<String, ExtractorServiceAdapter> adapterRegistry = new HashMap<String, ExtractorServiceAdapter>();
 	private Map<String, Model> models = new HashMap<String, Model>();
 	private Collection<ExtractorServiceAdapter> services;
+	private Configuration config;
 
 	public static void main(String args[]) {
-		DescriberAndExtractor describerAndExtractor = new DescriberAndExtractor();
+		try {
+			Configuration config = new Configuration(args);
 
-		if (args.length > 0) {
 			/*
-			 * configure a single endpoint
+			 * Check if endpoint file exists
 			 */
-			String endpointUri = args[0];
-			describerAndExtractor.addEndpoint(endpointUri);
-		} else {
 			/*
-			 * Fetch a list of endpoints
+			 * File endpointFile = config.getEndpointFile(); if
+			 * (!endpointFile.exists()) { System.err.println("The given file \""
+			 * + endpointFile + "\" does not exist!"); return; }
 			 */
-			describerAndExtractor.fetchListOfEndpoints(10, true);
+
+			DescriberAndExtractor describerAndExtractor = new DescriberAndExtractor(
+					config);
+
+			describerAndExtractor.run();
+		} catch (HelpException e) {
+			// Do nothing. The help message was requested.
+		} catch (Exception e) {
+			System.err.println("Coud not extract data.");
+
+			System.err.println(e.getMessage());
+			e.printStackTrace();
 		}
-		describerAndExtractor.run();
 	}
 
-	public DescriberAndExtractor() {
-		this.baseNs = "http://metex.hcls.deri.org/";
+	public DescriberAndExtractor(Configuration config) {
+		this.config = config;
+		this.baseNs = config.getPropertyAsString(CONF_baseUri,
+				"http://metex.hcls.deri.org/");
+
 		/*
 		 * create a persistent model so we can store it for further requests
 		 */
+
+		VirtuosoConfiguration virtDsConf = config.getVirtuosoConfig();
+		
 		ds = new VirtuosoDataSource();
-		ds.setServerName("localhost");
-		ds.setPortNumber(1111);
-		ds.setUser("dba");
-		ds.setPassword("dba");
-		// "jdbc:virtuoso://localhost:1111", "dba", "dba"
+		ds.setServerName(virtDsConf.serverName);
+		ds.setPortNumber(virtDsConf.portNumber);
+		ds.setUser(virtDsConf.user);
+		ds.setPassword(virtDsConf.password);
 		siteModel = VirtModel.openDatabaseModel(baseNs, ds);
 	}
 
@@ -116,7 +152,12 @@ public class DescriberAndExtractor {
 		}
 	}
 
-	public void fetchListOfEndpoints(int limit, boolean printLists) {
+	public void fetchListOfEndpoints() {
+
+		int limit = config.getPropertyAsInt(CONF_endpointsFetchLimit, 10);
+		boolean printLists = config.getPropertyAsBoolen(
+				CONF_endpointsPrintLists, false);
+
 		Collection<EndpointListProviderAdapter> services = new ArrayList<EndpointListProviderAdapter>();
 		services.add(new DatahubAdapter());
 		services.add(new VoidStoreAdapter(Endpoint.datasetFetchProperties));
@@ -150,6 +191,22 @@ public class DescriberAndExtractor {
 
 	public void run() {
 
+		String endpointUri = config.getPropertyAsString(CONF_endpoint, null);
+		
+		System.err.println("The configured endpoint is: " + endpointUri);
+		
+		if (endpointUri != null) {
+			/*
+			 * configure a single endpoint
+			 */
+			addEndpoint(endpointUri);
+		} else {
+			/*
+			 * Fetch a list of endpoints
+			 */
+			fetchListOfEndpoints();
+		}
+		
 		services = new ArrayList<ExtractorServiceAdapter>();
 		services.add(getAdapter("datahub"));
 		services.add(getAdapter("voidstore"));
@@ -251,6 +308,9 @@ public class DescriberAndExtractor {
 			for (ExtractorServiceAdapter service : services) {
 				i++;
 				serviceName = service.getClass().getSimpleName();
+				Resource serviceLink = siteModel.createResource(service
+						.getServiceLink(endpoint.getUri()));
+				endpointResource.addProperty(RDFS.seeAlso, serviceLink);
 
 				System.err.print(i + "/" + num + " (" + serviceName + "): ");
 
@@ -281,13 +341,15 @@ public class DescriberAndExtractor {
 					success = -2;
 				}
 
+				Resource serviceResource = siteModel.createResource(service
+						.getServiceUri());
 				if (success > 0) {
 					statusResource.addProperty(VOIDX.availableAt,
-							service.getServiceUri());
+							serviceResource);
 					retMessage = "successful";
 				} else if (success < 0) {
 					statusResource.addProperty(VOIDX.unavailableAt,
-							service.getServiceUri());
+							serviceResource);
 					retMessage = "failed";
 				} else {
 					retMessage = "done or nothing available";
@@ -364,7 +426,8 @@ public class DescriberAndExtractor {
 	private void registerModelsAtOntoWiki() {
 		try {
 			int requestID = RandomUtils.nextInt();
-			URL ontoWikiUrl = new URL(this.baseNs + "jsonrpc/model/");
+			String owBaseUri = config.getPropertyAsString(CONF_owBaseUri, baseNs);
+			URL ontoWikiUrl = new URL(owBaseUri + "jsonrpc/model/");
 
 			JSONRPC2Session owSession = new JSONRPC2Session(ontoWikiUrl);
 
